15f8bc3da01cacc0b4489f28292d07b1ca691b55
Deepak <db5608174@gmail.com>
2026-04-25 23:48:15 +0530

    updated

--- static/errorFormate.js
+++ static/errorFormate.js
@@ -1,26 +1,26 @@
  function showToast(message, type = "info") {
-    let bgColor = "rgba(255, 255, 255, 0.05)";  
+    let bgColor = "rgb(255, 255, 255)";  
     
-    if (type === "success") bgColor = "rgb(0, 255, 60)";
-    if (type === "error") bgColor = "rgb(255, 0, 25)";   
-    if (type === "warning") bgColor = "rgb(225, 254, 4)";   
+    if (type === "success") bgColor = "rgb(253, 247, 247)";
+    if (type === "error") bgColor = "rgb(255, 255, 255)";   
+    if (type === "warning") bgColor = "rgb(255, 255, 255)";   
 
     Toastify({
         text: message,
-        duration: 3000,
-        close: true,
+        duration: 3500,
+        // close: true,
         gravity: "top",
         position: "right",
         stopOnFocus: true, 
-        // --- Custom Glass Effect Yahan Hai ---
+         
         style: {
             background: bgColor,
             backdropFilter: "blur(12px)",
             webkitBackdropFilter: "blur(12px)",
-            border: "1px solid rgba(255, 255, 255, 0.1)",
-            borderRadius: "12px",
-            color: "#faf8f8",
-            fontSize: "14px"
+            border: "4px solid rgba(26, 25, 25, 0.25)",
+            borderRadius: "10px",
+            color: "#000000",
+            fontSize: "15px",
         },
     }).showToast();
 }
